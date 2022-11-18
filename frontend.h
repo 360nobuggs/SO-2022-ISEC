818c09be31bfadee42dcaba315856c2875b6c16e
Guilherme2s <118371826+Guilherme2s@users.noreply.github.com>
2022-11-18 18:29:45 +0000

    teste 1 para comonicacao

--- frontend.h
+++ frontend.h
@@ -2,7 +2,34 @@
 // Created by Vasco on 10/31/2022.
 //
 
-#ifndef SO_2022_ISEC_FRONTEND_H
-#define SO_2022_ISEC_FRONTEND_H
+#ifndef SO_2022_ISEC_BACKEND_H
+#define SO_2022_ISEC_BACKEND_H
+#include "communication.h"
+#include "users_lib.h"
+#include <wait.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <time.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <pthread.h>
+#include <string.h>
+#include <errno.h>
 
-#endif //SO_2022_ISEC_FRONTEND_H
+#define DEFAULT_MAXPLAYERS 10
+#define DEFAULT_LEILAODIR "."
+
+#define SERVER_FIFO "tmp/dict_fifo"
+#define CLIENT_FIFO "tmp/resp%d_fifo"
+#define TAM_MAX 50
+
+#endif //SO_2022_ISEC_BACKEND_H
